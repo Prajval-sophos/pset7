7d1aefb71c6155e45a1d4fe0ce8b8eefa8935648
Prajval Kumar <prajval1089@gmail.com>
2016-12-21 22:59:35 +0000

    Implemented Structure and Display of portfolio.

--- public/index.php
+++ public/index.php
@@ -3,7 +3,30 @@
     // configuration
     require("../includes/config.php"); 
 
+    $rows = CS50::query("SELECT symbol,shares FROM portfolios WHERE user_id=?",$_SESSION["id"]);
+    $positions = [];
+    foreach ($rows as $row)
+    {
+        $stock = lookup($row["symbol"]);
+        if ($stock !== false)
+        {
+            $positions[] = [
+                "name" => $stock["name"],
+                "price" => $stock["price"],
+                "shares" => $row["shares"],
+                "symbol" =>strtoupper($row["symbol"]),
+                "total" => ($stock["price"] * $row["shares"])
+            ];
+        }
+    }
+    
+    $users = CS50::query("SELECT cash FROM users WHERE id=? ",$_SESSION["id"]);
+    foreach ($users as $user)
+    {
+        $cash = $user["cash"];
+    }
+    $cash = $user["cash"];
     // render portfolio
-    render("portfolio.php", ["title" => "Portfolio"]);
-
+    render("portfolio.php", ["positions" => $positions, "title" => "Portfolio","total" => $cash]);
+    
 ?>
